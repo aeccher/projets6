d514ed081f7a528e0d79588842ae850469518019
aeccher <95070759+aeccher@users.noreply.github.com>
2024-01-26 10:51:50 +0100

    page d'accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,6 +1,17 @@
 <template>
-  <main>
-    <h1 class="font-playfair text-9xl">Eni’com</h1>
+  <main class="h-[900px] bg-vert">
+    <div class="relative">
+      <img src="/src/assets/images/Bulle.webp" alt="Image d'une bulle de conversation" class="m-auto w-4/6 pt-10" />
+      <div class="absolute inset-0 flex items-center justify-center">
+        <h1 class="z-50 font-playfair text-9xl text-blanc">Eni’com</h1>
+      </div>
+    </div>
+
+    <Router-Link to="/consignes">
+      <button class="font-Lato text-desktop m-auto flex h-10 items-center justify-center rounded-lg bg-orange px-20 text-blanc">
+        Commencer
+      </button></Router-Link
+    >
   </main>
 </template>
 
