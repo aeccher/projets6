7b1e5414c3a4e7c08d4fb6c979c3f387a503a1e3
aeccher <95070759+aeccher@users.noreply.github.com>
2024-01-26 11:23:29 +0100

    page explication/consignes

--- src/views/ConsignesView.vue
+++ src/views/ConsignesView.vue
@@ -1,1 +1,33 @@
-<template></template>
+<template>
+  <main class="bg-blanc">
+    <div class="m-auto mt-10 w-11/12 border-2 border-jaune">
+      <h2 class="px-8 pt-10 font-playfair text-6xl text-noir">Explication et consignes pour vous aider à résoudre les énigmes</h2>
+      <p class="px-8 pt-10 font-lato text-sm">
+        Depuis des milliers d’années, les hommes utilisent des codes et des alphabets spéciaux pour communiquer entre eux. Bien que ces
+        codes soient tous différents, ils visent tous à améliorer la communication entre les êtres humains.
+      </p>
+      <p class="px-8 pt-2 font-lato text-sm">
+        Ce jeu vous permettra de faire connaissance avec certains codes différents utilisés. En effet, en jouant à ce jeu votre mission va
+        consister à résoudre des énigmes en décryptant des textes codés, trouver un code secret, ...
+      </p>
+      <p class="px-8 pt-2 font-lato text-sm">Dans chaque énigme, vous trouverez un indice qui vous aidera à la résoudre.</p>
+      <div class="flex">
+        <p class="px-8 pt-2 font-lato text-sm">Pour obtenir un indice il faudra cliquer sur :</p>
+        <IconIndice class="pt-2" />
+      </div>
+      <Router-Link to="/enigme1">
+        <button class="font-Lato m-auto mb-16 mt-28 flex h-10 items-center justify-center rounded-lg bg-jaune px-20 text-noir">
+          Jouer
+        </button></Router-Link
+      >
+    </div>
+  </main>
+</template>
+
+<script>
+import IconIndice from "/src/components/icons/IconIndice.vue";
+
+export default {
+  components: { IconIndice },
+};
+</script>
---
+++ src/views/Enigme1View.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2 class="font-playfair text-6xl text-noir">Énigme n°1</h2>
+</template>
+<script></script>
