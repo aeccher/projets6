a9844ef564d9ddd9424c49bddc4b448a930ee6a0
aeccher <95070759+aeccher@users.noreply.github.com>
2024-01-26 10:51:50 +0100

    page d'accueil

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
+import ConsignesView from '../views/ConsignesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
-    // ici les autre routes
+    { path: '/consignes', name: 'Consignes', component: ConsignesView },
   ]
 })
 
