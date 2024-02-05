47f7569280fff1b64b8099a85a6be6322a1233c4
aeccher <95070759+aeccher@users.noreply.github.com>
2024-02-05 18:42:01 +0100

    Adaptation de la page de consignes en format mobile

--- src/views/ConsignesView.vue
+++ src/views/ConsignesView.vue
@@ -1,7 +1,9 @@
 <template>
   <main class="">
     <div class="m-auto mt-10 w-11/12 border-2 border-jaune">
-      <h2 class="px-8 pt-10 font-playfair text-6xl text-noir">Explication et consignes pour vous aider à résoudre les énigmes</h2>
+      <h2 class="px-8 pt-10 font-playfair text-3xl text-noir md:text-6xl">
+        Explication et consignes pour vous aider à résoudre les énigmes
+      </h2>
       <p class="px-8 pt-10 font-lato text-sm">
         Depuis des milliers d’années, les hommes utilisent des codes et des alphabets spéciaux pour communiquer entre eux. Bien que ces
         codes soient tous différents, ils visent tous à améliorer la communication entre les êtres humains.
@@ -11,13 +13,13 @@
         à résoudre des énigmes en décryptant des textes codés, trouver un code secret, associer des mots à leur langue ...
       </p>
       <p class="px-8 pt-2 font-lato text-sm">Dans chaque énigme, vous trouverez un indice qui vous aidera à la résoudre.</p>
-      <div class="flex">
+      <div class="grid md:flex">
         <p class="px-8 pt-2 font-lato text-sm">Pour obtenir un indice il faudra cliquer sur :</p>
-        <IconIndice class="pt-2" />
+        <IconIndice class="ml-10 pt-2 md:ml-0" />
       </div>
-      <div class="flex">
+      <div class="grid md:flex">
         <p class="px-8 pt-4 font-lato text-sm">Pour revenir à la page qui donne accès à toute les énigmes, il faudra cliquer sur :</p>
-        <IconRetour />
+        <IconRetour class="ml-10 md:ml-0" />
       </div>
       <p class="px-8 pt-4 font-lato text-sm">N'hésitez pas à utliser les zones pour écrire si besoin.</p>
       <Router-Link to="/enigmes">
